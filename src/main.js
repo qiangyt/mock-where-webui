045c1c30092ba5723acec57a646df9c6d94fbfc5
Qiang Yiting <qiangyt@wxcount.com>
2017-09-27 19:35:39 +0800

    go on

--- src/main.js
+++ src/main.js
@@ -6,23 +6,22 @@ import router from './router'
 import store from './store'
 import * as filters from './filters' // 全局filter
 import './icons' // icon
-import './errorLog'// error log
-import './permission' // 权限
-import './mock'  // 该项目所有请求使用mockjs模拟
+import './errorLog' // error log
+import './mock' // 该项目所有请求使用mockjs模拟
 
 Vue.use(ElementUI)
 
 // register global utility filters.
 Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
+    Vue.filter(key, filters[key])
 })
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    store,
+    template: '<App/>',
+    components: { App }
 })
